7b9e339567d707d79ba43ea7b95269c8fad36a49
U15383319 <3171372619@qq.com>
2024-11-28 12:21:05 +0800

    add comment to config

--- src/main/java/com/gvxwsur/tamabletool/common/config/TamableToolConfig.java
+++ src/main/java/com/gvxwsur/tamabletool/common/config/TamableToolConfig.java
@@ -15,14 +15,15 @@ public class TamableToolConfig {
         final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
         builder.comment("Tamable Tool Config");
         builder.push("General Settings");
-        needModAssistItem = builder.define("needModAssistItem", false);
-        modAssistItem = builder.define("modAssistItem", "minecraft:clock");
-        cheatTameItem = builder.define("cheatTameItem", "minecraft:debug_stick");
-        showTamableMessage = builder.define("showTamableMessage", true);
+        needModAssistItem = builder.comment("Whether an item should be held in the player's assisting hand to perform interactions in this mod").define("needModAssistItem", false);
+        modAssistItem = builder.comment("The item that held in the player's assisting hand to perform interactions in this mod").define("modAssistItem", "minecraft:clock");
+        cheatTameItem = builder.comment("The item that used to tame mobs without any cost").define("cheatTameItem", "minecraft:debug_stick");
+        showTamableMessage = builder.comment("Whether messages should be sent to players when something happened to their pets").define("showTamableMessage", true);
         builder.pop();
         builder.push("Tamable Permission");
-        compatibleVanillaTamable = builder.define("compatibleVanillaTamable", false);
-        compatiblePartEntity = builder.define("compatiblePartEntity", false);
+        builder.comment("Some of these configs are dangerous and increase the risk of mods conflicts, please use them with caution");
+        compatibleVanillaTamable = builder.comment("Whether vanilla tamable mobs will use taming system of this mod").define("compatibleVanillaTamable", false);
+        compatiblePartEntity = builder.comment("Whether multipart mobs will use taming system of this mod").define("compatiblePartEntity", true);
         builder.pop();
         CFG = builder.build();
     }
