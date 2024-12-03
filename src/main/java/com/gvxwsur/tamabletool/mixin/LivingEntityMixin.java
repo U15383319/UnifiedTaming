a9e220f43f84ff14abe1ab6a1573cabc23d9680e
U15383319 <3171372619@qq.com>
2024-12-03 10:17:05 +0800

    small change

--- src/main/java/com/gvxwsur/tamabletool/mixin/LivingEntityMixin.java
+++ src/main/java/com/gvxwsur/tamabletool/mixin/LivingEntityMixin.java
@@ -15,25 +15,26 @@ import javax.annotation.Nullable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity implements Attackable, IForgeLivingEntity, NeutralEntity {
-    @Shadow public abstract void setLastHurtByPlayer(@org.jetbrains.annotations.Nullable Player p_21248_);
 
     @Shadow protected int lastHurtByPlayerTime;
 
-    @Shadow public abstract void setLastHurtByMob(@org.jetbrains.annotations.Nullable LivingEntity p_21039_);
-
     @Shadow private int lastHurtByMobTimestamp;
+
+    @Shadow @Nullable protected Player lastHurtByPlayer;
+
+    @Shadow @Nullable private LivingEntity lastHurtByMob;
 
     public LivingEntityMixin(EntityType<?> p_19870_, Level p_19871_) {
         super(p_19870_, p_19871_);
     }
 
     public void tamabletool$setLastHurtByPlayer(@Nullable Player pPlayer, int time) {
-        this.setLastHurtByPlayer(pPlayer);
+        this.lastHurtByPlayer = pPlayer;
         this.lastHurtByPlayerTime = time;
     }
 
     public void tamabletool$setLastHurtByMob(@Nullable LivingEntity p_21039_, int timestamp) {
-        this.setLastHurtByMob(p_21039_);
+        this.lastHurtByMob = p_21039_;
         this.lastHurtByMobTimestamp = timestamp;
     }
 }
